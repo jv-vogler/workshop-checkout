34e04541db22164960c0693d0b6f20ee260a4ca1
Paulo Souza <paulo.souza@codeminer42.com>
2025-11-06 09:25:16 -0300

    WIP

--- src/core/cart.ts
+++ src/core/cart.ts
@@ -1,31 +1,107 @@
-import type { LineItem } from '@/core/lineItem'
-
-const TAX_RATE = 0.08
-const FREE_SHIPPING_THRESHOLD = 100
-const DEFAULT_SHIPPING_VALUE = 9.99
-const FREE_SHIPPING_VALUE = 0
+import { Product } from './product'
 
 /* eslint-disable @typescript-eslint/no-namespace */
 export namespace Cart {
+  export type LineItem = {
+    product: Product.Type
+    quantity: number
+  }
+
   export type Type = {
-    lineItems: Array<LineItem.Type>
+    lineItems: Array<LineItem>
   }
 
-  export const calculateTax = (subtotal: number): number => {
-    return subtotal * TAX_RATE
+  class CartEntityError extends Error {}
+
+  export namespace Errors {
+    export class NonPositiveQuantityError extends CartEntityError {}
+
+    export class ProductOutOfStockError extends CartEntityError {}
+
+    export class ProductQuantityExceedsStockError extends CartEntityError {}
+
+    export class LineItemNotFoundError extends CartEntityError {}
   }
 
-  export const calculateShipping = (subtotal: number): number => {
-    return subtotal >= FREE_SHIPPING_THRESHOLD
-      ? FREE_SHIPPING_VALUE
-      : DEFAULT_SHIPPING_VALUE
+  export const create = (): Type => {
+    return {
+      lineItems: [],
+    }
   }
 
-  export const isEmpty = (lineItems: Array<LineItem.Type>) => {
-    return lineItems.length === 0;
+  export const getLineItemQuantity = (
+    cart: Type,
+    productId: number,
+  ): number | undefined => {
+    return cart.lineItems.find((item) => item.product.id === productId)
+      ?.quantity
   }
 
-  export const createEmptyCart = (): Cart.Type => {
-    return { lineItems: [] }
+  export const addItem = (
+    cart: Type,
+    product: Product.Type,
+    quantity: number,
+  ): Type => {
+    if (quantity <= 0) throw new Errors.NonPositiveQuantityError()
+
+    if (Product.getStockStatus(product) === Product.StockStatus.OutOfStock)
+      throw new Errors.ProductOutOfStockError()
+
+    const lineItemQuantity = getLineItemQuantity(cart, product.id) ?? 0
+
+    if (lineItemQuantity + quantity > product.stock)
+      throw new Errors.ProductQuantityExceedsStockError()
+
+    const lineItemIndex = cart.lineItems.findIndex(
+      (item) => item.product.id === product.id,
+    )
+
+    const updatedLineItems = structuredClone(cart.lineItems)
+
+    if (lineItemIndex !== -1) {
+      updatedLineItems[lineItemIndex].quantity += quantity
+    } else {
+      updatedLineItems.push({ product, quantity })
+    }
+    return { ...cart, lineItems: updatedLineItems }
+  }
+
+  export const removeItem = (cart: Type, productId: number): Type => ({
+    ...cart,
+    lineItems: cart.lineItems.filter((item) => item.product.id !== productId),
+  })
+
+  export const getSubtotal = (cart: Type): number =>
+    cart.lineItems.reduce(
+      (sum, item) => sum + item.product.price * item.quantity,
+      0,
+    )
+
+  export const getTax = (cart: Type): number => getSubtotal(cart) * 0.08
+
+  export const getShipping = (cart: Type): number =>
+    getSubtotal(cart) > 100 ? 0 : 9.99
+
+  export const getTotal = (cart: Type): number =>
+    getSubtotal(cart) + getTax(cart) + getShipping(cart)
+
+  export const setLineItemQuantity = (
+    cart: Type,
+    productId: number,
+    quantity: number,
+  ): Type => {
+    if (quantity <= 0) throw new Errors.NonPositiveQuantityError()
+
+    const lineItemIndex = cart.lineItems.findIndex(
+      (item) => item.product.id === productId,
+    )
+
+    if (lineItemIndex === -1) throw new Errors.LineItemNotFoundError()
+
+    const updatedLineItems = structuredClone(cart.lineItems)
+
+    updatedLineItems[lineItemIndex].quantity = quantity
+
+    return { ...cart, lineItems: updatedLineItems }
   }
 }
